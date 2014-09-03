dba61bbfcc6208c299f6cb577d25e220a9fddabc
feroshjacob <feroshjacob@gmail.com>
2014-09-03 12:44:43 -0400

    Changed to include ID

--- src/main/scala/cb/index/IndexHelper.scala
+++ src/main/scala/cb/index/IndexHelper.scala
@@ -23,8 +23,8 @@ object IndexHelper {
   val indexPath = "cbIndex"
   val version = Version.LUCENE_4_9
 
-  def createIndex(list: List[(Int, String)], dir: Directory) = {
-    def indexRecipes(writer: IndexWriter, f: (Int, String)) = {
+  def createIndex[T](list: List[(T, String)], dir: Directory) = {
+    def indexRecipes(writer: IndexWriter, f: (T, String)) = {
       val doc = new Document();
       doc.add(new TextField("content", new StringReader(f._2)));
       doc.add(new StringField("documentID", f._1 + "", Field.Store.YES));
@@ -55,18 +55,18 @@ object IndexHelper {
     val results = searcher.search(query, 200);
     results.scoreDocs
   }
-  def searchText(text: String, reader: Directory): List[(Int, String, Float)] = {
+  def searchText(text: String, reader: Directory): List[(String, String, Float)] = {
     val searcher = new IndexSearcher(DirectoryReader.open(reader));
 
-    for (i <- getScoreDocs(text, reader).toList.map(f => (searcher.doc(f.doc).get("documentID").toInt, searcher.doc(f.doc).get("content"), f.score))) yield i
+    for (i <- getScoreDocs(text, reader).toList.map(f => (searcher.doc(f.doc).get("documentID"), searcher.doc(f.doc).get("content"), f.score))) yield i
 
   }
 
-  val threshold = 0.95f
+  val threshold = 0.97f
     val NonSpaceSymbols = """[\~\`\!\@\#\$\%\^\&\*\(\)\-\_\+\=\{\}\[\]\|\\\/\:\;\"\'\<\>\,\.\?]"""
-  def getCurrentCluster(searchResult: List[(Int, String, Float)]) = {
+  def getCurrentCluster[T](searchResult: List[(T, String, Float)]) = {
     require(!searchResult.isEmpty, "Search result should not empty, atleast the document itself should be returned")
-    val init: (List[Int], Float) = (List(),  searchResult.head._3)
+    val init: (List[T], Float) = (List(),  searchResult.head._3)
     searchResult.foldLeft(init)((p, q) => {
 
      
@@ -79,10 +79,10 @@ object IndexHelper {
   def main(args: Array[String]) {
     //		val list = List((1,"This is a boy"),(2, "This is a boy and girl") , (3,"This is so cute"))
 
-    var lineN = 0
+   
     val list = Source.fromFile("dataportaldata.txt").getLines.map(f => {
-      lineN += 1
-      (lineN, f)
+      val parts = f.split("\\t", 2)
+     ( parts(0),parts(1))
     }).toList
 
     val directory = new RAMDirectory()
